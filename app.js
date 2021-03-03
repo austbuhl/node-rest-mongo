0f8763fb2b42ea2b178c62c0a7ba42ce1f710e19
AB <austinbuhler@gmail.com>
2021-03-03 13:55:06 -0600

    Defining routes

--- app.js
+++ app.js
@@ -3,23 +3,20 @@ const mongoose = require('mongoose')
 const postsRouter = require('./routes/posts')
 require('dotenv').config()
 
+// Setup
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
-
+// Middleware
+app.use(express.json())
 app.use('/posts', postsRouter)
 
-// DB Config
+// DB Connection
 mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-const db = mongoose.connection
-console.log(db)
-
+// Start server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
--- routes/posts.js
+++ routes/posts.js
@@ -3,11 +3,50 @@ const router = express.Router()
 const Post = require('../models/Post')
 
 router.get('/', (req, res) => {
-  res.send('Posts index route')
+  Post.find((err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send(err)
+    } else {
+      console.log(data)
+      res.status(200).json(data)
+    }
+  })
 })
 
 router.post('/', (req, res) => {
   const post = req.body
+  Post.create(post, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).json(data)
+    }
+  })
+})
+
+router.get('/:postId', (req, res) => {
+  const postId = req.params.postId
+  Post.findById(postId, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).json(data)
+    }
+  })
+})
+
+router.put('/:postId', (req, res) => {
+  const postId = req.params.postId
+  const post = req.body
+
+  Post.findByIdAndUpdate(postId, post, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).json(data)
+    }
+  })
 })
 
 module.exports = router
